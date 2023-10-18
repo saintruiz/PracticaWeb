58a2ea416c6c178fa5afa693255949dac64862de
saintruiz <saruizt@unal.edu.co>
2023-10-18 17:30:56 -0500

    clase apartamento_duplex

--- web.py
+++ web.py
@@ -8,4 +8,25 @@ def funcion_principal():
         return "Perfecto, empieza el viaje"
     elif opciones==2:
         return "Regrese cuando esté listo"
-print(funcion_principal())
+class apartamento_duplex():
+    def __init__(self):
+        self.__numero_pisos=2
+        self.__numero_cocina=1
+        self.__numero_parq=2
+
+    def usado(self,usado):
+        self.usado=usado
+        if self.usado:
+            return "El apartamento es usado"
+        else:
+            return "El apartamento es nuevo"
+        
+    def estado(self):
+        print(f"El apartamento tiene {self.__numero_pisos} pisos, {self.__numero_cocina} cocinas y  {self.__numero_parq} parqueaderos")
+
+apartamento1=apartamento_duplex()
+print(apartamento1.usado(True))
+apartamento1.estado()
+
+
+# print(funcion_principal())
