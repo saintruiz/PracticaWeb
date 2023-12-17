4f7e17a77e8e09f710833dfb7de23533ab5ec4e8
saintruiz <saruizt@unal.edu.co>
2023-12-17 14:57:16 -0500

    Clase apartamento agregada

--- web.py
+++ web.py
@@ -1,32 +1,10 @@
-def funcion_principal():
-    opciones=int(input("""BIENVENIDO A ESTE PROYECTO INMOBILIARIO
-    Quiere seguir?
-    1. Sí
-    2. No
-    Ingrese opción: """))
-    if opciones == 1:
-        return "Perfecto, empieza el viaje"
-    elif opciones==2:
-        return "Regrese cuando esté listo"
-class apartamento_duplex():
-    def __init__(self):
-        self.__numero_pisos=2
-        self.__numero_cocina=1
-        self.__numero_parq=2
+class apartamento():
+    def __init__(self, nombrePropietario:str, idInmueble:str, estadoApto:str):
+        self.nombrePropietario=nombrePropietario
+        self.idInmueble=idInmueble
+        self.estadoApto=estadoApto
 
-    def usado(self,usado):
-        self.usado=usado
-        if self.usado:
-            return "El apartamento es usado"
-        else:
-            return "El apartamento es nuevo"
-        
-    def estado(self):
-        print(f"El apartamento tiene {self.__numero_pisos} pisos, {self.__numero_cocina} cocinas y  {self.__numero_parq} parqueaderos")
-
-apartamento1=apartamento_duplex()
-print(apartamento1.usado(True))
-apartamento1.estado()
-
-
-# print(funcion_principal())
+    def cambioEstado(self):
+        if self.estadoApto=="Nuevo":
+            self.estadoApto="Usado"
+    
